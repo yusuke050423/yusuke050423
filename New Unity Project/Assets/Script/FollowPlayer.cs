1e29ad8ff7453617f78e3289abdc3223ba7b9a1c
yusukeshida <yusuke050423@gmail.com>
2017-03-25 17:04:58 +0900

    3/25Unity勉強会

---
+++ New Unity Project/Assets/Script/FollowPlayer.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public Transform target;    // ターゲットへの参照
+
+    void Update()
+    {
+        // 自分の座標にtargetの座標を代入する     
+        GetComponent<Transform>().position = target.position;
+    }
+}
